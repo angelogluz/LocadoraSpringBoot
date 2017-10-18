885a5103a9c86b49f07322a137974e7fac0343e7
William Maich <williammaich@hotmail.com>
2017-10-18 19:44:23 -0200

    feito alguns testes

--- src/test/java/local/locadora/entities/TesteFilme.java
+++ src/test/java/local/locadora/entities/TesteFilme.java
@@ -8,6 +8,7 @@ package local.locadora.entities;
 import local.locadora.dao.FilmeDAO;
 import local.locadora.exceptions.FilmeSemEstoqueException;
 import local.locadora.exceptions.FilmeSemNomeException;
+import local.locadora.exceptions.FilmeSemPrecoException;
 import static org.hamcrest.CoreMatchers.is;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -60,16 +61,26 @@ public class TesteFilme {
   private Filme filme;
   
   @Test
-  public void testeFilme() throws FilmeSemNomeException, FilmeSemEstoqueException{
+  public void testeFilme() throws FilmeSemNomeException, FilmeSemEstoqueException, FilmeSemPrecoException{
     when(filme.setNome("macgayver")).thenReturn("Filme com nome");
-    when(filme.setNome("")).thenReturn("filme sem nome");
+    when(filme.setNome("")).thenReturn("Filme sem nome");
     when(filme.setEstoque(3)).thenReturn("Tem em estoque");
-    when(filme.setEstoque(0)).thenReturn("Sem estoque");
-    when(filme.setEstoque(4)).thenReturn("Filme com preço");
-    when(filme.setEstoque(0)).thenReturn("Filme sem preço");
+    when(filme.setEstoque(0)).thenReturn("Filme sem estoque");
+    when(filme.setPrecoLocacao(4.8)).thenReturn("Filme com preço");
+    when(filme.setPrecoLocacao(0.0)).thenReturn("Filme sem preço");
+   
     
-    
+    Assert.assertEquals("Filme com nome", filme.setNome("macgayver"));
+    Assert.assertEquals("Filme sem nome", filme.setNome(""));
+    Assert.assertEquals("Tem em estoque", filme.setEstoque(3));
+    Assert.assertEquals("Filme sem estoque", filme.setEstoque(0));
+    Assert.assertEquals("Filme com preço", filme.setPrecoLocacao(4.8));
+    Assert.assertEquals("Filme sem preço", filme.setPrecoLocacao(0.0));
 }
-    
+   
+  
+ 
+  
+  
     
 }
