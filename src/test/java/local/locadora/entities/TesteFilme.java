ba9a7d0a8ca66f44ea03c8e1ccc8376d52718346
William Maich <williammaich@hotmail.com>
2017-10-17 23:29:37 -0200

    feito os testes de cadastro de filmes

---
+++ src/test/java/local/locadora/entities/TesteFilme.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package local.locadora.entities;
+
+import local.locadora.dao.FilmeDAO;
+import local.locadora.exceptions.FilmeSemEstoqueException;
+import local.locadora.exceptions.FilmeSemNomeException;
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import static org.junit.Assert.assertThat;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ *
+ * @author william
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)
+
+public class TesteFilme {
+    
+    @Autowired
+    private FilmeDAO clientRepository;
+
+    public TesteFilme() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+    
+  @Mock
+  private Filme filme;
+  
+  @Test
+  public void testeFilme() throws FilmeSemNomeException, FilmeSemEstoqueException{
+    when(filme.setNome("macgayver")).thenReturn("Filme com nome");
+    when(filme.setNome("")).thenReturn("filme sem nome");
+    when(filme.setEstoque(3)).thenReturn("Tem em estoque");
+    when(filme.setEstoque(0)).thenReturn("Sem estoque");
+    when(filme.setEstoque(4)).thenReturn("Filme com preço");
+    when(filme.setEstoque(0)).thenReturn("Filme sem preço");
+    
+    
+}
+    
+    
+}
