b268b7ca3001e732546030f94a8fa286e828669d
Leandro - <leandrosantos2206@gmail.com>
2018-12-07 19:40:11 -0200

    Teste em ClienteEntity

--- src/test/java/local/locadora/entities/ClienteEntityTest.java
+++ src/test/java/local/locadora/entities/ClienteEntityTest.java
@@ -48,5 +48,46 @@ public class ClienteEntityTest {
 
         assertThat(message, is("Um nome deve possuir entre 4 e 50 caracteres"));
     }
+    
+    @Test
+    public void validarSeNomeTemNumerosOuSimbolos() {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Leandro !123");
+        Set<ConstraintViolation<Cliente>> violations = validator.validate(cliente);
+        Iterator it = violations.iterator();
+        //while(it.hasNext()){
+        ConstraintViolationImpl x = (ConstraintViolationImpl) it.next();
+        String message = x.getMessage();
+        // }
+        assertThat(message, is("O nome não deve possuir simbolos ou números"));
+    }
+
+    @Test
+    public void validarCPF() {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Leandro");
+        cliente.setCpf("123456789");
+        Set<ConstraintViolation<Cliente>> violations = validator.validate(cliente);
+        Iterator it = violations.iterator();
+        //while(it.hasNext()){
+        ConstraintViolationImpl x = (ConstraintViolationImpl) it.next();
+        String message = x.getMessage();
+        // }
+        assertThat(message, is("O CPF não é válido"));
+    }
+
+    @Test
+    public void naoValidarSeCPFEmBranco() {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Leandro");
+        cliente.setCpf("");
+        Set<ConstraintViolation<Cliente>> violations = validator.validate(cliente);
+        Iterator it = violations.iterator();
+        //while(it.hasNext()){
+        ConstraintViolationImpl x = (ConstraintViolationImpl) it.next();
+        String message = x.getMessage();
+        // }
+        assertThat(message, is("O CPF não é válido"));
+    }
 }
 
