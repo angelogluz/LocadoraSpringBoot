b4d9415f90c0b255a72662c97c5cc68f8f590fa5
JPWALLY2 <jacowally8@gmail.com>
2018-12-08 09:38:20 -0200

    Testes

--- src/test/java/local/locadora/entities/ClienteEntityTest.java
+++ src/test/java/local/locadora/entities/ClienteEntityTest.java
@@ -48,5 +48,18 @@ public class ClienteEntityTest {
 
         assertThat(message, is("Um nome deve possuir entre 4 e 50 caracteres"));
     }
+    
+     @Test
+    public void naoDeveValidarUmNomeComNumeroseSimbolos() {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Wally !123");
+        Set<ConstraintViolation<Cliente>> violations = validator.validate(cliente);
+        Iterator it = violations.iterator();
+        //while(it.hasNext()){
+        ConstraintViolationImpl x = (ConstraintViolationImpl) it.next();
+        String message = x.getMessage();
+        // }
+        assertThat(message, is("O nome não deve possuir simbolos ou números"));
+    }
 }
 
