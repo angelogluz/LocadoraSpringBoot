91e42f77a0985ab5201327e8c0ef12584ee7aa41
Aluno do Laboratório <aluno@fatecpel.senacrs.com.br>
2018-12-10 20:34:01 -0200

    Enviando os testes

---
+++ src/test/java/local/locadora/entities/FilmeEntityTestjava.java
@@ -0,0 +1,94 @@
+package local.locadora.entities;
+
+import org.hibernate.validator.internal.engine.ConstraintViolationImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Iterator;
+import java.util.Set;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FilmeEntityTest {
+
+    private static Validator validator;
+
+    @Before
+    public void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+    
+    @Test
+    public void filmeDeveTerEntreDoisECemCaracteres() {
+        Filme filme = new Filme();
+        filme.setNome("a");
+        Set<ConstraintViolation<Filme>> violations = validator.validate(filme);
+        Iterator it = violations.iterator();
+        //while(it.hasNext()){
+        ConstraintViolationImpl x = (ConstraintViolationImpl) it.next();
+        String message = x.getMessage();
+        // }
+
+        assertThat(message, is("Um filme deve possuir entre 2 e 100 caracteres"));
+    }
+    
+    public void naoDeveValidarUmNomeComMaisDeCemCaracteres() {
+         Filme filme = new Filme();
+        filme.setNome("testesabcdefghijklmnopqrstuvxzABCDEFGHIJKLMNOPQRSTUVXZTestestestesabcdefghijklmnopqrstuvxzABCDEFGHIJKLMNOPQRSTUVXZTestes");
+        
+          Set<ConstraintViolation<Filme>> violations = validator.validate(filme);
+        Iterator it = violations.iterator();
+        ConstraintViolationImpl x = (ConstraintViolationImpl) it.next();
+        String message = x.getMessage();
+        assertThat(message, is("Um filme deve possuir entre 2 e 100 caracteres"));
+    }
+    
+    @Test
+    public void naoDeveAceitarEspacoEmBrancoNoInicioEnoFim() {
+        Filme filme = new Filme();
+        filme.setNome("  Filme  ");
+        assertThat(filme.getNome(), is("Filme"));
+    }
+
+    @Test
+    public void estoqueDeveSerPositivo() {
+        Filme filme = new Filme();
+        filme.setEstoque(-1);
+        Set<ConstraintViolation<Filme>> violations = validator.validate(filme);
+        Iterator it = violations.iterator();
+        ConstraintViolationImpl x = (ConstraintViolationImpl) it.next();
+        String msg = x.getMessage();
+
+        assertThat(msg, is("O Estoque tem que ser positivo"));
+    }
+
+    @Test
+    public void precoDeveTerNoMaximoDoisDigitos() {
+        Filme filme = new Filme();
+        filme.setPrecoLocacao(123.321);
+        Set<ConstraintViolation<Filme>> violations = validator.validate(filme);
+        Iterator it = violations.iterator();
+        ConstraintViolationImpl x = (ConstraintViolationImpl) it.next();
+        String msg = x.getMessage();
+
+        assertThat(msg, is("O Preço deve ter no máximo dois dígitos"));
+    }
+
+    @Test
+    public void valorDaLocacaoDeveSerPositivo() {
+        Filme filme = new Filme();
+        filme.setPrecoLocacao(-10.2);
+        Set<ConstraintViolation<Filme>> violations = validator.validate(filme);
+        Iterator it = violations.iterator();
+        ConstraintViolationImpl x = (ConstraintViolationImpl) it.next();
+        String msg = x.getMessage();
+
+        assertThat(msg, is("O Valor da locação deve ser positivo"));
+    }
+}
